be32650452e547cdbd8192456d7347d66740b4a9
unknown <UFG@CC6-08.ufg.local>
2024-05-28 19:46:01 -0600

    Práctica Finalizada con la toma de fotos

--- app/src/main/java/sv/edu/ufg/fis/amb/contentProviderHernandezGuido/MainActivity.kt
+++ app/src/main/java/sv/edu/ufg/fis/amb/contentProviderHernandezGuido/MainActivity.kt
@@ -1,11 +1,55 @@
 package sv.edu.ufg.fis.amb.contentProviderHernandezGuido
 
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.BlockedNumberContract
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import java.io.File
+import java.net.URL
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var captura : ImageView
+    private lateinit var urlImagen : Uri
+    private lateinit var tiempo: String
+    private lateinit var nombreImagen : String
+
+    private var contract = registerForActivityResult(ActivityResultContracts.TakePicture()){
+        captura.setImageURI(null)
+        captura.setImageURI(urlImagen)
+    }
+
+    private fun createImageUri():Uri {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            tiempo = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss").withZone(ZoneOffset.UTC).format(Instant.now())
+        }
+
+        nombreImagen = "$tiempo.png"
+
+        val imagen = File(filesDir,nombreImagen)
+
+        return FileProvider.getUriForFile(this,"sv.edu.ufg.fis.amb.contentProviderHernandezGuido.fileProvide",imagen)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        captura = findViewById(R.id.captureImageView)
+        val captureImgBtn = findViewById<Button>(R.id.captureImgBtn)
+
+        captureImgBtn.setOnClickListener{
+            urlImagen = createImageUri()
+            contract.launch(urlImagen)
+        }
     }
 }
